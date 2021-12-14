f06216409c82a2478ab95c860bee38a24c091ad8
ANTONIOa7o <78384046+ANTONIOa7o@users.noreply.github.com>
2021-12-14 18:59:00 +0100

    Add files via upload

---
+++ ejemplo3-directivas/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+    alumnos: any = [
+      {valoracion: 'alta', repetidor: false, nombre: 'Juan', apellido: 'Lopez', nota: 7.5},
+      {valoracion: 'media', repetidor: false, nombre: 'Maria', apellido: 'Sanchez', nota: 5.8},
+      {valoracion: 'baja', repetidor: true, nombre: 'Elena', apellido: 'Arias', nota: 3.2},
+      {valoracion: 'media', repetidor: true, nombre: 'Roberto', apellido: 'Rodriguez', nota: 6.4},
+      {valoracion: 'baja', repetidor: false, nombre: 'Javier', apellido: 'Martin', nota: 4.1},
+      {valoracion: 'alta', repetidor: false, nombre: 'Marta', apellido: 'Gonzalez', nota: 8.3}
+    ];
+}
